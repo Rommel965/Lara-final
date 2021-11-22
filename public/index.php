2dbde6508cee280c9b08d0d33b2869aa0b6eabbd
Rommel965 <5865@itescam.edu.mx>
2021-11-21 21:17:57 -0600

    prueba

--- public/index.php
+++ public/index.php
@@ -20,6 +20,10 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
+<form action="ejemplo.php">
+  <p>Nombre: <input type="text" name="nombre"></p>
+  <p><input type="submit" value="Enviar"></p>
+</form>
 
 require __DIR__.'/../vendor/autoload.php';
 
