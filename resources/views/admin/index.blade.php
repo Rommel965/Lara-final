31d37b962de6245b5129b8c6a709ec609111b7b5
KarlaHA <72232278+KarlaHA@users.noreply.github.com>
2021-11-25 23:33:51 -0600

    pruebas

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,7 +7,35 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+<section>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <input type="text" id="input" placeholder="Nombre">
+    </div>
+    <div class="col-sm">
+    <input type="text" id="input" placeholder="Status">
+    </div>
+    <div class="col-sm">
+            <select name="" id="">
+                <option selected disabled="">Carrera</option>
+                <option value="">Ing. Sistemas</option>
+                <option value="">Ing. Informática</option>
+                <option value="">Ing. Materiales</option>
+                <option value="">Ing. Mecatrónica</option>
+                <option value="">Ing. Bioquímica</option>
+                <option value="">Ing. Innovación agrícola</option>
+                <option value="">Lic. Administración</option>
+                <option value="">Lic. Industrial</option>
+            </select>
+   
+    </div>
+    <div class="col-sm">
+    <button class="btn btn-primary" type="button">Nuevo</button>
+    </div>
+  </div>
+</div>
+    </section>
 @stop
 
 @section('css')
