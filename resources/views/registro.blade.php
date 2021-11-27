a9bb577d7b9e434cb5540772b2b33929bdf2550d
KarlaHA <72232278+KarlaHA@users.noreply.github.com>
2021-11-26 23:30:07 -0600

    cambios

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -10,7 +10,16 @@
 <div class="container">
   <div class="row">
     <div class="col-sm">
-    <input type="text" id="input" placeholder="Nombre">
+    <input type="text" id="input" placeholder="Nombre de la empresa"><br></br>
+    <input type="text" id="input" placeholder="Representante">
+    </div>
+
+    <div class="col-sm">
+  
+    </div>
+
+    <div class="col-sm">
+    <button class="btn btn-primary" type="button">guardar</button>
     </div>
 @stop
 
